f146cb95670b4fad427e2dd78bcb6beadbfacabb
Butterroach <butterroach@proton.me>
2024-05-07 22:25:02 +0300

    bit of settings

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const statustext = document.getElementById("status");
+const intensity = document.getElementById("intensity");
+const mobilemode = document.getElementById("mobile");
 let firefox = typeof navigator.userAgentData === "undefined"; // ! FIREFOX MIGHT ADD THIS IN THE FUTURE
 if (firefox) {
     statustext.textContent = "firefox isnt compatible with the vibrator :(";
 }
 
-async function vibrate(gamepad) {
+async function vibrate(gamepad, intensity) {
     return await gamepad.vibrationActuator.playEffect("dual-rumble", {
         startDelay: 0,
         duration: 1.0,
-        weakMagnitude: 1.0,
-        strongMagnitude: 1.0,
+        weakMagnitude: intensity,
+        strongMagnitude: intensity,
     });
 }
 
@@ -25,8 +27,11 @@ window.addEventListener("gamepadconnected", async function (event) {
         statustext.textContent = "enjoy the vibrator! >///<";
         while (true) {
             console.log("ran");
-            vibrate(selectedGamepad);
-            await wait(10);
+            vibrate(selectedGamepad, intensity.value / 100);
+            await wait(
+                (mobilemode.checked ? 50 : 10) /
+                    ((intensity.value ? intensity.value : 1) / 50)
+            );
         }
     } else {
         statustext.textContent =
